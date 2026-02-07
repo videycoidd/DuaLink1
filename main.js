0ec7c05cb7c7ed9715ee6402c8530adf9c25db95
videycoidd <asupancadangan@gmail.com>
2026-02-07 13:29:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ shareBtn.onclick = () => {
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://s.shopee.co.id/4VWbicjGrp");
+    window.open("https://whatsapp.com/channel/0029VbCCtde3WHTdWYywNk2m");
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/profile.php?id=61583489829473");
+    window.open("https://s.shopee.co.id/5VNNZGN1Kk");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -34,6 +34,7 @@ overlay.addEventListener("click", () => {
     // Aksi setelah klik
     window.open("https://s.shopee.co.id/4VWbicjGrp", "_blank");
 });
+
 
 
 
